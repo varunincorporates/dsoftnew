a199474978df1a6dd6b58d337d03298a3f94a415
Sunil JANGIRA <suniljangira@hotmail.com>
2020-07-29 11:52:01 +0530

    tweaked app for heroku

--- telusko/settings.py
+++ telusko/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'telusko.urls'
@@ -122,6 +126,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedMainfestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assests')
 
 MEDIA_URL = "/media/"
@@ -138,3 +144,6 @@ EMAIL_HOST_PASSWORD = 'crjyzteymdoaaeox'
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_USE_SSL = False
+
+django_heroku.settings(locals())
+
