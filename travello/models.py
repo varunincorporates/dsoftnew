55e302fd3daf18a557a649d6d473153d887ed8fd
Sunil Jangira <suniljangira2005@gmail.com>
2020-08-16 19:38:28 +0530

    complains updates sms

--- travello/models.py
+++ travello/models.py
@@ -6,6 +6,44 @@ from embed_video.fields import EmbedVideoField
 from django.contrib.auth.models import User, auth
 
 
+class Newcomplain(models.Model):
+    user1 = models.ForeignKey(User, blank=True,  on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, blank=False)
+    mobile = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(max_length=60)
+    accountno = models.CharField(max_length=100, blank=False)
+    STATUS = {
+        ('No Connection', 'No Connection'),
+        ('Slow Speed', 'Slow Speed'),
+        ('Billing Complant', 'Billing Complant'),
+        ('Relocation', 'Relocation'),
+    }
+    category = models.CharField(max_length=200, null=True, choices=STATUS)
+    STATUS1= {
+        ('UNABLE TO BROWSE','UNABLE TO BROWSE'),
+        ('PASSWORD FORGOT','PASSWORD FORGOT'),
+        ('WEBSITE NOT OPENING','WEBSITE NOT OPENING'),
+        ('CABLE CUT','CABLE CUT'),
+        ('EQUIPEMENT PROBLEM','EQUIPEMENT PROBLEM'),
+        ('FREQUENTLY DISCONNECT','FREQUENTLY DISCONNECT'),
+        ('ROUTER SUPPORT','ROUTER SUPPORT'),
+        ('BILL DISPUTE', 'BILL DISPUTE'),
+        ('PAYMENT NOT UPDATED','PAYMENT NOT UPDATED'),
+        ('PAYMENT PROBLEM','PAYMENT PROBLEM'),
+        ('SLOW SPEED TEST','SLOW SPEED TEST'),
+    }
+    subject = models.CharField(max_length=200, null=True, choices=STATUS1)
+    note = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True,default='PENDING')
+    date_solved = models.DateTimeField(null=True)
+    comments= models.CharField(max_length=500, null=True)
+
+    def __str__(self):
+        return self.id, self.name, self.mobile, self.note
+
+
+
 
 class Newcustomer(models.Model):
     name = models.CharField(max_length=200, null=True)
@@ -173,38 +211,3 @@ class Myorder(models.Model):
         return self.name, self.product, self.note
 
 
-class Newcomplain(models.Model):
-    user1 = models.ForeignKey(User, blank=True,  on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, blank=False)
-    mobile = models.CharField(max_length=100, blank=False)
-    email = models.EmailField(max_length=60)
-    accountno = models.CharField(max_length=100, blank=False)
-    STATUS = {
-        ('No Connection', 'No Connection'),
-        ('Slow Speed', 'Slow Speed'),
-        ('Billing Complant', 'Billing Complant'),
-        ('Relocation', 'Relocation'),
-    }
-    category = models.CharField(max_length=200, null=True, choices=STATUS)
-    STATUS1= {
-        ('UNABLE TO BROWSE','UNABLE TO BROWSE'),
-        ('PASSWORD FORGOT','PASSWORD FORGOT'),
-        ('WEBSITE NOT OPENING','WEBSITE NOT OPENING'),
-        ('CABLE CUT','CABLE CUT'),
-        ('EQUIPEMENT PROBLEM','EQUIPEMENT PROBLEM'),
-        ('FREQUENTLY DISCONNECT','FREQUENTLY DISCONNECT'),
-        ('ROUTER SUPPORT','ROUTER SUPPORT'),
-        ('BILL DISPUTE', 'BILL DISPUTE'),
-        ('PAYMENT NOT UPDATED','PAYMENT NOT UPDATED'),
-        ('PAYMENT PROBLEM','PAYMENT PROBLEM'),
-        ('SLOW SPEED TEST','SLOW SPEED TEST'),
-    }
-    subject = models.CharField(max_length=200, null=True, choices=STATUS1)
-    note = models.TextField()
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
-    status = models.CharField(max_length=200, null=True,default='PENDING')
-    date_solved = models.DateTimeField(null=True)
-    comments= models.CharField(max_length=500, null=True)
-
-    def __str__(self):
-        return self.id, self.name, self.mobile, self.note
