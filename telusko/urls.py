c7dcda1a9460e1ec07319a95b29b1e309fa6dfd5
Sunil Jangira <suniljangira2005@gmail.com>
2020-09-23 08:11:05 +0530

    new

--- telusko/urls.py
+++ telusko/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
   path('', include('travello.urls')),
   path('admin/', admin.site.urls),
-  path('accounts/', include('accounts.urls')),
   path('travello/', include('travello.urls')),
 
 ]
